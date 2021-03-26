5c1a731cfa8ea9ce19954f05f7b0e1d0edea22ef
dandihara <dandihara@gmail.com>
2021-03-26 14:58:39 +0900

    기초틀 완성

---
+++ src/components/movie.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({year, title,summary, poster, genres}){
+    return(
+    <div class = "movie">
+        <img src={poster} alt = {title} title = {title}/>
+        <div class = "movie_data">
+            <h3 class = "movie_title">{title}</h3>
+            <h5 class = "movie_year">{year}</h5>
+            <ul className = "genres">
+                {genres.map((genre , index )  => 
+                <li key = {index} className = "genres_genre">{genre}</li>
+                )}
+            </ul>
+            <p class = "movie_summary">{summary.slice(0,180)}...</p>
+        </div>
+    </div>
+    );
+}
+
+Movie.propTypes = {
+    id :PropTypes.number.isRequired,
+    year : PropTypes.number.isRequired,
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
